c9d4644761357a293e8cf742f2137b66c323c872
Flvrset <mkominiak11@gmail.com>
2023-01-21 16:07:16 +0100

    test commit :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script>
+	let a = 1;
+
+	function func() {
+		a++;
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="divers">
+    <span> testing </span>
+	<button on:click={func}>{a}</button>
+</div>
+
+<style>
+	.divers {
+		background-color: lightblue;
+        display: flex;
+        border-radius: 8px;
+        width: auto;
+        justify-content: center;
+	}
+</style>

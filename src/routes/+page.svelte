987de8ea14895380fbafaeb748ea8550171868da
Flvrset <mkominiak11@gmail.com>
2023-02-01 18:12:15 +0100

    Start working on a site since now i finally have some spare time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,46 @@
 <script>
-	const getRandomUser = async () => {
-		var response = await fetch('https://randomuser.me/api/');
-		var result = await response.json();
-		return result;
-	};
-
-	let userPromise = getRandomUser();
+	import Hero from '$lib/assets/hero.jpg';
 </script>
 
-{#await userPromise}
-	<h2>Loading...</h2>
-{:then users}
-	<h2>{users.results[0].name.first}</h2>
-{:catch err}
-	<h2>xd??</h2>
-{/await}
+<div class="hero">
+	<nav>
+		<ul>
+			<li>O mnie</li>
+			<li>Doświadczenie</li>
+			<li>Kontakt</li>
+		</ul>
+	</nav>
+	<div class="container">
+		<div class="hero">Joanna Kominiak</div>
+		<div class="hero__image"><img src={Hero} style="size: 2px;" alt="hero" /></div>
+	</div>
+</div>
+<section class="aboutme" />
+
+<style>
+	:global(body) {
+		display: contents;
+		margin: 0;
+		padding: 0;
+		height: 100%;
+	}
+	.hero__image {
+		max-width: 10%;
+		height: auto;
+		border-radius: 8px;
+	}
+	.hero {
+		background-color: whitesmoke;
+	}
+	.aboutme {
+		height: 100vh;
+		background-color: aquamarine;
+	}
+	.container {
+		width: 100%;
+		padding-left: 15px;
+		padding-right: 15px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>

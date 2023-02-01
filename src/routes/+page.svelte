17f07cf590ec9191d2d82990d3fac8ec41b918ee
Flvrset <mkominiak11@gmail.com>
2023-02-01 18:31:28 +0100

    add sass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
 	import Hero from '$lib/assets/hero.jpg';
+	9 ? 's' : 'b';
 </script>
 
 <div class="hero">
@@ -11,27 +12,42 @@
 		</ul>
 	</nav>
 	<div class="container">
-		<div class="hero">Joanna Kominiak</div>
-		<div class="hero__image"><img src={Hero} style="size: 2px;" alt="hero" /></div>
+		<div class="hero">
+			<div class="hero__about--top">Joanna Kominiak</div>
+			<div class="hero__image">
+				<img src={Hero} style="size: 2px;" alt="hero" />
+			</div>
+		</div>
 	</div>
 </div>
 <section class="aboutme" />
 
-<style>
+<style lang="scss">
 	:global(body) {
 		display: contents;
 		margin: 0;
 		padding: 0;
 		height: 100%;
 	}
-	.hero__image {
-		max-width: 10%;
-		height: auto;
+	img {
+		width: 500px;
 		border-radius: 8px;
 	}
+
 	.hero {
 		background-color: whitesmoke;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 50px;
+
+		&__about {
+			&--top {
+				font-size: 50px;
+			}
+		}
 	}
+
 	.aboutme {
 		height: 100vh;
 		background-color: aquamarine;

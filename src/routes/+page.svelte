0990177e6f76123fbcd6bf5cf01cbdf9e0f9b5b0
Miłosz Kominiak <mkominiak11@gmail.com>
2023-02-03 18:49:34 +0100

    Start styling fonts <3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,14 +12,23 @@
 <nav>
 	<ul class="navbar">
 		<a href="#hero" on:click|preventDefault={scrollIntoView}>Home</a>
-		<a href="#about-me" style="margin-left: 30px" on:click|preventDefault={scrollIntoView}>O mnie!</a>
-		<a href="#experience" style="margin-left: 30px" on:click|preventDefault={scrollIntoView}>Doświadczenie</a>
+		<a href="#about-me" style="margin-left: 30px" on:click|preventDefault={scrollIntoView}
+			>O mnie!</a
+		>
+		<a href="#experience" style="margin-left: 30px" on:click|preventDefault={scrollIntoView}
+			>Doświadczenie</a
+		>
 		<a href="#contact" on:click|preventDefault={scrollIntoView}>Kontakt</a>
 	</ul>
 </nav>
 <section class="container" id="hero">
 	<div class="hero">
-		<div class="hero__about--top">Joanna Kominiak</div>
+		<div class="hero__about">
+			<span class="hero__about--top">Joanna Kominiak</span>
+			<span class="hero__about--bottom"
+				>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span
+			>
+		</div>
 		<div class="hero__image">
 			<img src={Hero} style="size: 2px;" alt="hero" />
 		</div>
@@ -66,12 +75,22 @@
 		gap: 50px;
 
 		&__about {
+			display: flex;
+			flex-direction: column;
+			gap: 15px;
 			&--top {
-				font-size: 50px;
+				font-family: Ivar Text, sans-serif;
+				font-weight: 600;
+				font-size: 70px;
+				letter-spacing: -0.2px;
+			}
+			&--bottom {
+				font-family: 'DM Sans', sans-serif;
+				font-weight: 400;
 			}
 		}
 		img {
-			width: 500px;
+			width: 200px;
 			border-radius: 8px;
 		}
 	}
@@ -85,11 +104,33 @@
 		justify-content: center;
 		align-items: center;
 
-		width: 100%;
+		width: 800px;
 		padding-left: 15px;
 		padding-right: 15px;
 		margin-left: auto;
 		margin-right: auto;
 		height: 100vh;
+	}
+
+	@font-face {
+		font-family: 'Ivar Text';
+		src: url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.eot');
+		src: url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.eot?#iefix')
+				format('embedded-opentype'),
+			url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.woff2') format('woff2'),
+			url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.woff') format('woff'),
+			url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.ttf') format('truetype'),
+			url('//db.onlinewebfonts.com/t/22aff4497483f2c6a5e001453850b935.svg#Ivar Text Hydro')
+				format('svg');
+	}
+	@font-face {
+		font-family: 'DM Sans';
+		src: url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.eot');
+		src: url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.eot?#iefix')
+				format('embedded-opentype'),
+			url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.woff2') format('woff2'),
+			url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.woff') format('woff'),
+			url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.ttf') format('truetype'),
+			url('//db.onlinewebfonts.com/t/d9d77150b3dda1eea88a7a8f979e7711.svg#DM Sans') format('svg');
 	}
 </style>

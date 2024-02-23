075f63d0542b422e7026e104c2812c133ff16ba2
Miłosz Kominiak <mkominiak11@gmail.com>
2024-02-23 09:43:19 +0100

    add vercel analytics

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,4 +1,9 @@
+<script lang="ts">
+	import {dev} from '$app/environment';
+	import {inject} from '@vercel/analytics';
 
+	inject({mode: dev ? 'development' : 'production'});
+</script>
 
 <div class="navbar">
 	<a href="/">HOME</a>
